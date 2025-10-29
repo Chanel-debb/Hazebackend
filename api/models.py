37bc30448110eaac64c71be4f8fd91634489bef7
Deborah <deborah@Chanels-MacBook-Air.local>
2025-10-29 16:08:52 +0100

    first commit

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Vistors(models.Model):
+    fullname = models.CharField(max_length=100)
+    phone_no = models.JSONField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    signed_in = models.DateTimeField(null=True, blank=True)
+    signed_out = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.fullname
