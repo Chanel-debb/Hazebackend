ad792913e6e5d1864039f4eae6138df65d91dc71
Deborah <deborah@Chanels-MacBook-Air.local>
2025-12-12 22:23:49 +0100

    Fix permission classes to use lowercase role values

--- user/permissions.py
+++ user/permissions.py
@@ -3,21 +3,21 @@ from rest_framework.permissions import BasePermission
 
 class IsAdminUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.role == "Admin")
+        return bool(request.user and request.user.role == "admin")
     
 
 class IsModeratorUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.role == "Moderator")
+        return bool(request.user and request.user.role == "moderator")
     
 
 class IsBaseUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.role == "BaseUser")
+        return bool(request.user and request.user.role == "resident")
     
 
 class IsAdminOrModeratorUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and (request.user.role == "Admin" or request.user.role == "Moderator"))
+        return bool(request.user and request.user.role in ["admin", "security"])  
     
 
